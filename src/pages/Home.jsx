6c224f68ee1b0e18a0e964db72b00a807dc21e5e
adityackr <aditya09me@gmail.com>
2022-08-04 21:44:05 +0600

    feat: add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import pic from '../assets/images/my-pic.png';
 import Layout from '../components/layout/Layout';
 
 const Home = () => {
-	return <Layout>Home</Layout>;
+	return (
+		<Layout>
+			<div className="w-full py-4 bg-bgImage">
+				<div className="container mx-auto flex justify-evenly items-center">
+					<img src={pic} alt="aditya" />
+					<div className="text-white">
+						<h1 className="text-7xl mb-5 font-bold">I'm Aditya Chakraborty</h1>
+						<h2 className="text-5xl mb-5 text-[#0089C3]">
+							Passionate Full Stack Web Development Learner
+						</h2>
+						<h3 className="text-3xl mb-10">
+							I will be available soon for your service
+						</h3>
+						<div>
+							<Link
+								className="p-3 font-bold bg-transparent hover:bg-white hover:text-[#0089C3] hover:duration-300 border rounded-md border-[#0089C3]"
+								to="/about"
+							>
+								About me
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				bgImage: "url('/src/assets/bg/bg.svg')",
+			},
+		},
 	},
 	plugins: [],
 };
